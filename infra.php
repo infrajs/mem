89bef544d9d58115014f3c52bdc59697c70a0ed5
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-07 16:12:40 +0400

    ns

--- infra.php
+++ infra.php
@@ -1,7 +1,14 @@
 <?php
 namespace infrajs\mem;
-use infrajs\infra\Config;
+use infrajs\event\Event;
+use infrajs\infra\Infra;
+use infrajs\path\Path;
 
-$conf=Infra::config('mem');
-
+$conf=&Infra::config('mem');
 Mem::$conf=array_merge(Mem::$conf, $conf);
+$conf=Mem::$conf;
+
+Event::wheng('update', function () {
+	Path::mkdir(Mem::$conf['cache']);
+});
+
