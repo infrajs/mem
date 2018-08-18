640a33f7b41867dd2205a9cec694bd0091638714
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-08-18 20:27:13 +0400

    time sort -mem/

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 use infrajs\rest\Rest;
 use infrajs\mem\Mem;
 use akiyatkin\fs\FS;
+use infrajs\ans\Ans;
 
 Rest::get( function (){
 	$mem = Mem::memcache();
@@ -12,19 +13,29 @@ Rest::get( function (){
 		echo '<pre>';
 		print_r($ar);
 	} else {
-		echo 'Используется файловая система<br>';
+		echo 'Используется файловая система<br><form method="post"><input type="hidden" name="clear" value="yes"><button type="submit">Очистить</button></form>';
 		echo Mem::$conf['cache'];
 		echo '<hr>';
 		$size = 0;
 		$list = array();
+		
+		if (Ans::REQ('clear')) {
+			FS::scandir(Mem::$conf['cache'], function($file) {
+				FS::unlink(Mem::$conf['cache'].$file);
+			});	
+		}
+
 		FS::scandir(Mem::$conf['cache'], function($file) use (&$size, &$list){
-			$list[] = $file;
+
+			$list[$file] = FS::filemtime(Mem::$conf['cache'].$file);
 			$size += FS::filesize(Mem::$conf['cache'].$file);
 		});
+		
+		arsort($list);
 		echo round($size/1000000,2).' Mb<br>';
 		echo sizeof($list).' записей';
 		echo '<pre>';
-		foreach ($list as $file) {
+		foreach ($list as $file => $key) {
 			echo '<a href="/'.Mem::$conf['cache'].$file.'">'.$file.'</a>'."\n";
 		}
 
